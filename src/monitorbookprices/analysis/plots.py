a458cf095de168efeee98fedc51ffb060bcd5abb
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2024-10-28 23:12:21 +0100

    feat(plots): Improve plots

--- src/monitorbookprices/analysis/plots.py
+++ src/monitorbookprices/analysis/plots.py
@@ -9,28 +9,70 @@ def plot_history(
     df,
     site=None,
     title=None,
+    book=None,
     height=300,
     width=800,
-    hline=None,
+    hlines=None,
 ):
     """Plot history of recent price recordings."""
     if site is not None:
         df = df.filter(pl.col('site') == site)
+    plot_title = (
+        title
+        if title is not None
+        else None
+        if book is None
+        else f"{book['isbn'][0]}: {book['author'][0]} - {book['title'][0]}. {book['publisher'][0]} ({book['year'][0]})"
+    )
     plo = df.hvplot.line(
         x='date',
         y='price',
         by='site',
-        title=title,
+        title=plot_title,
         height=height,
         width=width,
         rot=90,
     )
-    if hline is not None:
-        line = hv.HLine(hline)
-        line.opts(
-            color='black',
-            line_dash='dashed',
-            line_width=0.5,
-        )
-        plo *= line
+
+    if hlines is not None:
+        for hline in hlines:
+            if type(hline) is str:
+                if book is None:
+                    raise ValueError('Book row not defined.')
+                if hline == 'min':
+                    plo *= horizontal_line(
+                        df['date'].min(),
+                        df['date'].max(),
+                        book['min_price'][0],
+                        label='min',
+                        # line_width=0.6,
+                        line_dash='dashed',
+                    )
+                if hline in ['max', 'full']:
+                    plo *= horizontal_line(
+                        df['date'].min(),
+                        df['date'].max(),
+                        book['full_price'][0],
+                        label='max',
+                        # line_width=0.6,
+                        line_dash='dashed',
+                    )
+            if type(hline) is dict:
+                try:
+                    plo *= horizontal_line(
+                        hline['X_A'], hline['X_B'], hline['Y'], hline['label']
+                    )
+                except ValueError:
+                    raise ValueError('Wrong format hline.')
     return plo
+
+
+def horizontal_line(X_A, X_B, Y, label=None, line_width=1, line_dash='solid'):
+    """Horizontal line with label using holoviews Curve."""
+    return hv.Curve(
+        [[X_A, Y], [X_B, Y]],
+        label=label,
+    ).opts(
+        line_width=line_width,
+        line_dash=line_dash,
+    )
